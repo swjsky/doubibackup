cd8a1361472a89d0062afcc0f68ab291f0661637
wbqss <31788183+wbqss@users.noreply.github.com>
2018-05-19 00:03:38 +0800

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+export PATH;
 
 #=================================================
 #	System Required: Debian/Ubuntu
@@ -9,16 +9,12 @@ export PATH
 #	Author: Toyo
 #	Blog: https://doub.io/wlzy-18/
 #=================================================
-
-socat_file="/usr/bin/socat"
-socat_log_file="/tmp/socat.log"
+sh_ver="1.0.5";
+socat_file="/usr/bin/socat";
+socat_log_file="/tmp/socat.log";
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}" && Error="${Red_font_prefix}[错误]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-
-Save_iptables(){
-	iptables-save > /etc/iptables.up.rules
-}
+Info="${Green_font_prefix}[信息]${Font_color_suffix}" && Error="${Red_font_prefix}[错误]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}";
 Set_iptables(){
 	if [[ ${release} == "debian" ]]; then
 		iptables-save > /etc/iptables.up.rules
